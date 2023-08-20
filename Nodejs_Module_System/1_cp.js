ffdb9db6d0e7902762533801bb75488f77e6e1aa
sumanashettigar <87168235+sumanashettigar@users.noreply.github.com>
2023-08-20 18:00:56 +0530

    Update 1_cp.js

--- Nodejs_Module_System/1_cp.js
+++ Nodejs_Module_System/1_cp.js
@@ -1,8 +1,8 @@
 const cp = require('child_process')//importing child process module
 
+cp.execSync('calc') //to display calcultor
 
+cp.execSync('start chrome') // To start a chrome
+cp.execSync('start chrome https://www.google.com/') // To open a particular web page
 
-//cp.execSync('calc')
-// cp.execSync('start chrome https://web.whatsapp.com/')
-
-console.log('output ' +cp.execSync('node demo.js'))
+console.log('output ' + cp.execSync('node demo.js')) //To run a javascript file and get the output of it
