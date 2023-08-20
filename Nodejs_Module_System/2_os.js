8ca9de6f53063dd0d67d6177f673faf740ec574e
sumanashettigar <87168235+sumanashettigar@users.noreply.github.com>
2023-08-20 18:02:52 +0530

    Update 2_os.js

--- Nodejs_Module_System/2_os.js
+++ Nodejs_Module_System/2_os.js
@@ -1,10 +1,7 @@
 const os = require('os')//importing libraries from operating system module
 
-
-// console.log(os.arch())
-
-// console.log(os.platform())
-
-// console.log(os.cpus())
-// console.log(os.totalmem())
-console.log(os.freemem())
+console.log(os.arch())//To display system architechture
+console.log(os.platform()) // To display the information of current platform
+console.log(os.cpus()) // To display CPU details
+console.log(os.totalmem())// To display  total memory
+console.log(os.freemem()) // To display memory used
